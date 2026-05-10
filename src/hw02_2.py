04b0d155a7e0901bc5d09edf5f81f751284aae9e
specialstr <03sihu@naver.com>
2026-05-10 17:47:59 +0900

    Initial commit

---
+++ src/hw02_2.py
@@ -0,0 +1,18 @@
+# Complete Project Details: https://RandomNerdTutorials.com/raspberry-pi-digital-inputs-python/
+
+# Import the Button and LED classes from the gpiozero library
+from gpiozero import Button, LED
+# Import pause to keep the program running
+from signal import pause
+
+# Create an LED object connected to GPIO pin 14
+led = LED(14)
+# Create a Button object connected to GPIO pin 4
+button = Button (4)
+
+# When the button is pressed, toggle the LED state
+# (If the LED is off, turn it on / If it is on, turn it off)
+button.when_pressed = led.toggle
+
+# Keep the script running and waiting for button events
+pause()
